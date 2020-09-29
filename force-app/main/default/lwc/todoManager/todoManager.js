a65798bb9877f0fe3af4964938bad7879b708a28
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2020-09-29 17:16:52 +0530

    Live timer added

--- force-app/main/default/lwc/todoManager/todoManager.js
+++ force-app/main/default/lwc/todoManager/todoManager.js
@@ -1,3 +1,55 @@
-import { LightningElement } from 'lwc';
+import {LightningElement,track} from 'lwc';
 
-export default class TodoManager extends LightningElement {}
+export default class TodoManager extends LightningElement {
+    time = "8:10 PM";
+    greeting = "Evening";
+
+    connectedCallback()
+    {
+        this.getTime();
+    }
+
+
+    getTime() {
+        const date = new Date();
+        const hour =  date.getHours();
+        const min =  date.getMinutes();
+
+        this.time =`${this.getHour(hour)}:${this.getDoubleDigit(min)} ${this.getMidDay(hour)}`;
+        this.getGriteeing(hour);
+    }
+
+    getHour(hour) {
+        return hour === 0 ? 12 : hour > 12 ? (hour - 12) : hour;
+    }
+    
+    getMidDay(hour)
+    {
+        return hour >= 12 ? "AM" : "PM";
+
+    }
+    getDoubleDigit(digit)
+    {
+        return digit < 10 ? "0"+digit : digit;
+    }
+    
+
+
+    getGriteeing(hour) 
+    {
+        if (hour < 12) 
+        {
+
+            this.greeting = "Good Morining";
+        } else if (hour >= 12 && hour < 17) 
+        {
+            this.greeting = "Good Afternoon";
+
+        } else 
+        {
+            this.greeting = "Good Evening";
+        }
+    }
+}
+
+//23:38 
