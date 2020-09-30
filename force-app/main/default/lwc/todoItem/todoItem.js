ee19b175042a030c4ba4ec4fe5c3d3a850ebd148
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2020-09-30 23:18:07 +0530

    todoitem updated

--- force-app/main/default/lwc/todoItem/todoItem.js
+++ force-app/main/default/lwc/todoItem/todoItem.js
@@ -2,5 +2,15 @@ import { LightningElement,api } from 'lwc';
 
 export default class TodoItem extends LightningElement 
 {
-    
+    @api todoId;
+    @api done = false;
+    @api todoName;
+
+
+
+    get containerClass()
+    {
+        return this.done ? "todo completed" : "todo upcomming"
+    }
+
 }
