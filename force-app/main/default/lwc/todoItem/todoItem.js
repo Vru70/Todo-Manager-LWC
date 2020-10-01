e403ee020fa5eeaa43e049c4a84ddd69a40e5bf3
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2020-10-01 23:53:29 +0530

    todo Items Icons chnages

--- force-app/main/default/lwc/todoItem/todoItem.js
+++ force-app/main/default/lwc/todoItem/todoItem.js
@@ -1,4 +1,7 @@
+import deleteTodo from '@salesforce/apex/ToDoController.deleteTodo';
+import updateTodo from '@salesforce/apex/ToDoController.updateTodo';
 import { LightningElement,api } from 'lwc';
+
 
 export default class TodoItem extends LightningElement 
 {
@@ -7,6 +10,31 @@ export default class TodoItem extends LightningElement
     @api todoName;
 
 
+    updateHandler()
+    {
+        const todo ={
+            todoId: this.todoId,
+            todoName: this.todoName,
+            done: !this.done
+        }; 
+
+        updateTodo({payload : JSON.stringify(todo)}).then(result =>{
+            console.log('Item Updated Successfully');
+        
+        }).catch(error =>{
+            console.error('Error in UPDATE', error );
+        });
+    }
+
+    deleteHandler()
+    {
+        deleteTodo({todoId: this.todoId}).then(result =>{
+            console.log('Item Deleted Successfully');
+           
+        }).catch(error =>{
+            console.error('Error in Delete', error );
+        });
+    }
 
     get containerClass()
     {
